2f9770030f571f0bb17750674d858e20b2ef6c54
Евгений Непомнящий <rush.zlo@gmail.com>
2021-12-19 03:04:04 +0300

    change commutation

--- rb.h
+++ rb.h
@@ -5,11 +5,22 @@
 
 class StepperControl {
 public:
+    StepperControl() {
+        ESP_LOGD("StepperControl", "CONSTRUCTOR");
+
+        max_speed_ = 0;
+        max_speed_ = 0;
+        xceleration_ = 0;
+    }
+
     /**
      * Setup stepper
      */
     void config(int32_t max_steps, int32_t max_speed, float xceleration)
-    {   
+    {
+        ESP_LOGD("StepperControl", "before: max_steps: %d, max_speed: %d, xceleration: %0.2f", max_steps_, max_speed_, xceleration_);
+        ESP_LOGD("StepperControl", "after:  max_steps: %d, max_speed: %d, xceleration: %0.2f", max_steps, max_speed, xceleration);
+
         max_steps_ = max_steps;
         max_speed_ = max_speed;
         xceleration_ = xceleration;
@@ -29,7 +40,9 @@ public:
         // d0 = v * v / (2 * a)
         // far from top -> allow more distance to stop
         // d1 = d0 * (cp / tp) = v * v / (2 * a) * (cp / tp)
-        
+
+        ESP_LOGD("StepperControl", "stop: max_steps: %d, max_speed: %d, xceleration: %0.2f", max_steps_, max_speed_, xceleration_);
+
         int brakepath = int(max_speed_ * max_speed_ / (2 * xceleration_) * xstepper->current_position / max_steps_);
 
         // how many upside/downside steps left to target?
